fe7a3e821629d4dd732d311d2747da62e3554f40
Yury <quasaryy@gmail.com>
2022-12-29 20:15:45 +0400

    Added logic

--- Rejoice/ViewController.swift
+++ Rejoice/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var resultTextLabel: UILabel!
+    @IBOutlet var resultButton: UIButton!
+    var range: Range<Date>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resultButton.layer.cornerRadius = 12
+        resultTextLabel.isHidden = true
     }
+    
 
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        guard sender.date <= Date.now else {
+            return
+        }
+        range = sender.date..<Date.now
+    }
+    
 
+    @IBAction func resultButtonAction() {
+        guard range != nil else {
+            resultTextLabel.isHidden = false
+            return resultTextLabel.text = "Please choose date before now"
+        }
+        let days = range.formatted(.components(style: .wide, fields: [.day]))
+        resultTextLabel.isHidden = false
+        resultTextLabel.text = "You have been enjoying life for \(days) days"
+    }
+    
 }
 
