95909a6e07a42f169c84196ac3bf7bdd7f846ab9
Yury <quasaryy@gmail.com>
2022-12-29 18:43:48 +0400

    Initial Commit

---
+++ Rejoice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rejoice
+//
+//  Created by Yury on 29.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
